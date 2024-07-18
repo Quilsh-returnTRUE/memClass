c229c23b93cb2e7db8dda5e3c88be65f62476d40
Quilsh-returnTRUE <quilsh5@gmail.com>
2024-07-18 18:23:26 -0500

    Update mem.hpp

--- mem.hpp
+++ mem.hpp
@@ -6,11 +6,24 @@
 
 namespace mem
 {
+    inline HANDLE pHandle;
+    inline uintptr_t procID;
 
-  uintptr_t get_proc_id(const wchar_t* processName);   // ex: cs2.exe
-  uintptr_t get_module_base(uintptr_t processID, const wchar_t* module);   // ex: client.dll | engine.dll
+    uintptr_t get_proc_id(const wchar_t* processName);   // ex: cs2.exe
+    uintptr_t get_module_base(uintptr_t processID, const wchar_t* module);   // ex: client.dll | engine.dll
 
-  template <typename R> R Read(uintptr_t address);
-  template <typename R> bool Write(uintptr_taddress, R value);
+
+    template <typename R> R ReadMem(uintptr_t address)
+    {
+        R mit;
+        ReadProcessMemory(pHandle, (LPCVOID)address, &mit, sizeof(R), nullptr);
+        return mit;
+    }
+
+
+    template <typename R> bool WriteToMem(uintptr_t address, R value)
+    {
+        return WriteProcessMemory(pHandle, (LPVOID)address, &value, sizeof(R), nullptr);
+    }
 
 }
