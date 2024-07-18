21d01a927e3fc3c748942295f402d8d2ec213190
Quilsh-returnTRUE <quilsh5@gmail.com>
2024-07-18 17:57:34 -0500

    Update mem.hpp

--- mem.hpp
+++ mem.hpp
@@ -1,1 +1,16 @@
 
+#pragma once
+
+#include <Windows.h>
+#include <TlHelp32.h>
+
+namespace mem
+{
+
+  uintptr_t get_proc_id(const wchar_t* processName);   // ex: cs2.exe
+  uintptr_t get_module_base(uintptr_t processID, const wchar_t* module);   // ex: client.dll | engine.dll
+
+  template <typename R> R Read(uintptr_t address);
+  template <typename R> bool Write(uintptr_taddress, R value);
+
+}
